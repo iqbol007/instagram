9b276906d082cc1d26cc6b492e5d85f1de69d094
iqbol <iqbol@localhost>
2020-02-12 17:34:04 +0300

    something Fixed

--- src/components/VievAllComments/VievAllComments.js
+++ src/components/VievAllComments/VievAllComments.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { POST_ADD } from "../../actions/actionTypes";
+import React from "react";
 import Comment from "../Comment/Comment";
 
 let dialogStyles = {
