fe31db7f06a66df16dbfaae4be8ec354137b0ba4
iqbol <iqbol@localhost>
2020-02-12 17:49:00 +0300

    something

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Comment from '../Comment/Comment'
 
 export default function Comments({ comments, dispatch }) {
-
     return (
         <div>
             {comments.map(o => <Comment key={o.id} comment={o} dispatch={dispatch} />).reverse()}
