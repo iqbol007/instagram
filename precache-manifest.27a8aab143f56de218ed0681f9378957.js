2cbe44970bd5f9ed0738463c16c9882bec7de269
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-12 14:36:09 +0000

    Deploy iqbol007/instagram to github.com/iqbol007/instagram.git:gh-pages

---
+++ precache-manifest.27a8aab143f56de218ed0681f9378957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59aaa0176cd5bfa7b17f01ef4d10541f",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "7ac97bc4f60ee9982fa6",
+    "url": "/instagram/static/css/main.29886b7a.chunk.css"
+  },
+  {
+    "revision": "6e87aa01fac4080c0a3f",
+    "url": "/instagram/static/js/2.13c10190.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/instagram/static/js/2.13c10190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac97bc4f60ee9982fa6",
+    "url": "/instagram/static/js/main.d9402add.chunk.js"
+  },
+  {
+    "revision": "5271fd828b9c5f7156b4",
+    "url": "/instagram/static/js/runtime-main.75c03d26.js"
+  }
+]);
